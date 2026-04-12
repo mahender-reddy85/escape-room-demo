99fd6a8184b08903e798b5f7368710e803ebcb72
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-04-12 17:51:19 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if (typeof window !== 'undefined' && !window.WebGLRenderingContext) {
+  alert("Your browser does not support WebGL. The 3D scene will not render correctly.");
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
